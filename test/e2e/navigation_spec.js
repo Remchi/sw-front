a1ca155444109d2c6310ee82e1e1f86d6af68f2a
Rem Zolotykh <remchi@gmail.com>
2014-06-10 19:06:35 +0300

    Fix protractor spec to work with latest API #2

--- test/e2e/navigation_spec.js
+++ test/e2e/navigation_spec.js
@@ -3,14 +3,12 @@
 describe('Navigation Menu', function() {
   it('changes active link depending on route', function() {
     browser.get('/');
-    var activeListItem = element(by.css('.active'));
-    var text = activeListItem.findElement(by.tagName('a')).getText();
+    var text = element(by.css('.active > a')).getText();
     expect(text).toEqual('Home');
 
     var edgesLink = element(by.linkText('Edges'));
     edgesLink.click();
-    activeListItem = element(by.css('.active'));
-    text = activeListItem.findElement(by.tagName('a')).getText();
+    text = element(by.css('.active > a')).getText();
     expect(text).toEqual('Edges');
 
   });
